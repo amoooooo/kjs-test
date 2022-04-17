d14c4065d2b2e54064411f26a9c844255e4bb7a5
amo <lunarteclipse@gmail.com>
2022-04-17 20:10:53 +0100

    jars

--- kubejs/server_scripts/rewrite.js
+++ kubejs/server_scripts/rewrite.js
@@ -16,7 +16,8 @@ onEvent('block.right_click', event => {
             let time = 200;
             let itemCount = 0;
             let fluidCount = 0;
-            let counter = 0;
+            let count = 0;
+            console.log(checkRecipes(inputItems, definedRecipe));
             if (checkRecipes(inputItems, definedRecipe)) {
                 console.log("Recipe found");
                 for (item in definedRecipe.items) {
@@ -24,11 +25,12 @@ onEvent('block.right_click', event => {
                     itemCount++;
                 }
                 for (fluid in definedRecipe.fluid) {
-                    time += 50;
+                    time += 60;
                     fluidCount++;
                 }
-                console.log(`Time: ${time} ticks per item: ${50 * itemCount} ticks per fluid: ${50 * fluidCount}`);
-                extractItemsInPedestals(event, event.getBlock().pos, -4, -2, -4, 4, 0, 4, time, itemCount, fluidCount);
+                count = definedRecipe.fluid[0].count;
+                console.log(`Time: ${time} ticks per item: ${50 * itemCount} ticks per fluid: ${60 * fluidCount}`);
+                extractItemsInPedestals(event, event.getBlock().pos, -4, -2, -4, 4, 0, 4, time, itemCount, fluidCount, count);
             }
         }
     }
@@ -87,18 +89,57 @@ function getItemsInPedestals(event, pos, x1, y1, z1, x2, y2, z2) {
     return { items: items, catalyst: catalyst, fluid: fluids };
 }
 
-function extractItemsInPedestals(event, pos, x1, y1, z1, x2, y2, z2, time, itemCount, fluidCount) {
+function extractItemsInPedestals(event, pos, x1, y1, z1, x2, y2, z2, time, itemCount, fluidCount, count) {
     let box = betweenClosed(event, pos, x1, y1, z1, x2, y2, z2);
     let dimension = event.level.dimension;
+    let indexPedestal = 0;
+    let indexJar = 0;
     event.server.runCommandSilent(`execute positioned ${pos.x} ${pos.y + 2} ${pos.z} in ${dimension} run playsound minecraft:block.portal.ambient block @a[distance=..5] ${pos.x} ${pos.y + 2} ${pos.z} 25 0.2`);
     box.forEach((block, idx, arr) => {
         if (block.id == "supplementaries:pedestal" && block.entity !== null) {
-            if (!block.pos.equals(pos.below(2))){
-                console.log(`Extracting item from ${block.pos}`);
-                extractPedestals(block, pos, event, dimension, time, idx);
+            if (!block.pos.equals(pos.below(2))) {
+                //console.log(`Extracting item from ${block.pos}`);
+                //console.log(`index: ${idx}`);
+                extractPedestals(block, pos, event, dimension, time, indexPedestal);
+                indexPedestal++;
             }
+        } else if (block.id == "supplementaries:jar" && block.entity !== null) {
+            //console.log(`Extracting fluid from ${block.pos}`);
+            extractJars(block, pos, event, dimension, time, indexJar, count);
+            indexJar++;
         }
     })
+}
+
+function extractJars(block, pos, event, dimension, time, index, count) {
+    let fluid = getFluid(block);
+    if (fluid !== "minecraft:empty") {
+        let currentTicks = 0;
+        event.server.scheduleInTicks(1, event => {
+            console.log(count);
+            if (currentTicks < time) {
+                if (index < 1) {
+                    if (currentTicks > count*10 * index && currentTicks <= (count*10) * (index + 1)) {
+                        console.log(`Extracting fluid ${fluid} from ${block.pos}`);
+                        particleLine(event, block, pos, getFluidColor(block), dimension);
+                    }
+                } else {
+                    if (currentTicks <= (count*10) * (index + 1)) {
+                        console.log(`Extracting fluid ${fluid} from ${block.pos}`);
+                        particleLine(event, block, pos, getFluidColor(block), dimension);
+                    }
+                }
+                if (currentTicks == count*10){
+                    let data = block.entityData;
+                    data.FluidHolder.Count = data.FluidHolder.Count - count;
+                    block.setEntityData(data);
+                    block.entity.setChanged();
+                }
+                currentTicks++;
+                event.reschedule();
+            }
+        })
+    }
 }
 
 function extractPedestals(block, pos, event, dimension, time, index) {
@@ -112,12 +153,12 @@ function extractPedestals(block, pos, event, dimension, time, index) {
             if (currentTicks < time) {
                 if (index > 1) {
                     if (currentTicks >= 50 * index && currentTicks <= 50 * (index + 1)) {
-                        console.log(`Extracting ${item}`);
+                        //console.log(`Extracting ${item}`);
                         event.server.runCommandSilent(`execute in ${dimension} run particle minecraft:item ${item} ${block.x} ${block.y + 1.5} ${block.z} ${vec.x} ${vec.y} ${vec.z} 0.175 0 normal`);
                     }
                 } else {
                     if (currentTicks <= 50 * (index + 1)) {
-                        console.log(`Extracting ${item}`);
+                        //console.log(`Extracting ${item}`);
                         event.server.runCommandSilent(`execute in ${dimension} run particle minecraft:item ${item} ${block.x} ${block.y + 1.5} ${block.z} ${vec.x} ${vec.y} ${vec.z} 0.175 0 normal`);
                     }
                 }
@@ -188,4 +229,42 @@ function betweenClosed(event, pos, x1, y1, z1, x2, y2, z2) {
 function vecToTarget(pos, target, speed) {
     let step = target.subtract(pos).multiply(1 / speed);
     return step;
+}
+
+function getFluidCount(block) {
+    return block.entityData.FluidHolder.Fluid;
+}
+
+function getFluidColor(block) {
+    return intToRGB(block.entityData.FluidHolder.CachedColor);
+}
+
+function getFluid(block) {
+    return block.entityData.FluidHolder.Fluid;
+}
+
+function intToRGB(col) {
+    const b = (col) & 0xFF;
+    const g = (col >> 8) & 0xFF;
+    const r = (col >> 16) & 0xFF;
+    return { r: r / 255, g: g / 255, b: b / 255 };
+}
+
+function particleLine(event, start, end, color, dimension) {
+    let d = global.functions.distance(start.x, start.y, start.z, end.x, end.y, end.z);
+    let counter = 0;
+    let i = -1;
+    for (let i = -1; i < d * 10; i++) {
+        let delta = i / 10 / d;
+        let x = (1 - delta) * (start.x + 0.5) + delta * (end.x + 0.5);
+        let y = (1 - delta) * (start.y + 1) + delta * (end.y + 0.5);
+        let z = (1 - delta) * (start.z + 0.5) + delta * (end.z + 0.5);
+        //console.log(`${x} | ${y} | ${z}`);
+        if (counter == 4) {
+            event.server.runCommandSilent(`execute in ${dimension} run particle minecraft:dust ${color.r} ${color.g} ${color.b} 1.0 ${x} ${y} ${z} 0.1 0.05 0.1 0 2 normal`);
+            counter = 0;
+        } else {
+            counter++;
+        }
+    }
 }
